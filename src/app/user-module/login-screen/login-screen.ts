ae1dc6eb7f8356f883480d2a541694825298548f
Pietro Henrique <pietrohenriquealvesdossantospi@gmail.com>
2025-09-03 21:57:11 -0300

    feat: update forms create page

--- src/app/user-module/login-screen/login-screen.ts
+++ src/app/user-module/login-screen/login-screen.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-screen',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-screen.html',
   styleUrl: './login-screen.css'
 })
 export class LoginScreen {
+
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder ) {
+    //quando a tela iniciar 
+    this.loginForm = this.fb.group({
+      email: [" ",[Validators.required] ],
+      password: [" ", Validators.required]
+    });
+  }
+
+  onLoginClick(){
+
+    alert("Botao clicado")
+    console.log("Email", this.loginForm.value.email)
+    console.log("Password", this.loginForm.value.password)
+
+  }
+
    
 }

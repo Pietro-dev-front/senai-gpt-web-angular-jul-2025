86179269b0802e67bf71d8bcc51714654340d921
Pietro Henrique <pietrohenriquealvesdossantospi@gmail.com>
2025-09-10 21:55:02 -0300

    feat: apdate

--- src/app/user-module/new-user-screen/new-user-screen.ts
+++ src/app/user-module/new-user-screen/new-user-screen.ts
@@ -1,23 +1,24 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { authGuard } from '../../auth.guard';
 
 @Component({
   selector: 'app-new-user-screen',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './new-user-screen.html',
   styleUrl: './new-user-screen.css'
 })
 export class NewUserScreen {
-  emailErrorMessage: string;
-  newPasswordErrorMessage: string;
-  passwordErrorMessage: string;
-  nomeMensagerError: string;
-  sucessLogin: string;
-  incorrectCredentials: string;
+  // emailErrorMessage: string;
+  // newPasswordErrorMessage: string;
+  // passwordErrorMessage: string;
+  // nomeMensagerError: string;
+  // sucessLogin: string;
+  // incorrectCredentials: string;
 
   newUserLoginForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, ) {
 
      this.newUserLoginForm = this.fb.group({
       name: ["",[Validators.required]],
@@ -26,50 +27,13 @@ export class NewUserScreen {
       newPassword: ["", [Validators.required]],
 
     });
-    this.emailErrorMessage = "";
-    this.passwordErrorMessage = "";
-    this.sucessLogin = "";
-    this.incorrectCredentials = "";
-    this.nomeMensagerError= "";
-    this.newPasswordErrorMessage="";
   }
 
   async onCadastro() {
-    this.emailErrorMessage = "";
-    this.passwordErrorMessage = "";
-    this.sucessLogin = "";
-    this.incorrectCredentials = "";
-    this.nomeMensagerError= "";
-    this.newPasswordErrorMessage="";
-
-    if (this.newUserLoginForm.value.name == "") {
-      this.emailErrorMessage = "O campo de nome e obrigatorio.";
-      return
-    }
-
-    if (this.newUserLoginForm.value.email == "") {
-      this.emailErrorMessage = "O campo de e-mail e obrigatorio.";
-      return
-    }
-
-     if (this.newUserLoginForm.value.newPassword == "") {
-      this.emailErrorMessage = "O campo de nova senha e obrigatorio.";
-      return
-    }
-
-    if (this.newUserLoginForm.value.password == "") {
-      this.passwordErrorMessage = "O campo de senha e obrigatorio.";
-      return
-    }
-
-    console.log("Email", this.newUserLoginForm.value.email)
-    console.log("Password", this.newUserLoginForm.value.password)
-
-    //this.cd.detectChanges();
-    //console.log("STATUS CODE ", response.status)
-
+    
     const token = localStorage.getItem("meuToken")
-    console.log(token)
+    console.log("cheguei",token)
+
     let response = await fetch("https://senai-gpt-api.azurewebsites.net/users", {
       method: "POST",
       headers: {
@@ -82,10 +46,7 @@ export class NewUserScreen {
         name: this.newUserLoginForm.value.name,
         newPassword: this.newUserLoginForm.value.newPassword,
       }),
-
     });
-
+    console.log("cheguei aqui ", authGuard())
   }
-
-
 }

013cb1d8ef9c0138aba5e369cf7c324d8d4ab32c
Pietro Henrique <pietrohenriquealvesdossantospi@gmail.com>
2025-09-12 21:50:50 -0300

    feat: create update token

--- src/app/user-module/new-user-screen/new-user-screen.ts
+++ src/app/user-module/new-user-screen/new-user-screen.ts
@@ -1,20 +1,21 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { authGuard } from '../../auth.guard';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-new-user-screen',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './new-user-screen.html',
   styleUrl: './new-user-screen.css'
 })
 export class NewUserScreen {
-  // emailErrorMessage: string;
-  // newPasswordErrorMessage: string;
-  // passwordErrorMessage: string;
-  // nomeMensagerError: string;
-  // sucessLogin: string;
-  // incorrectCredentials: string;
+  emailErrorMessage!: string;
+  passwordErrorMessage!: string;
+  sucessLogin!: string;
+  incorrectCredentials!: string;
+  nameErrorMessage!:string;
+  newPasswordErrorMessage!: string;
 
   newUserLoginForm: FormGroup;
 
@@ -25,15 +26,47 @@ export class NewUserScreen {
       email: ["", [Validators.required]],
       password: ["", [Validators.required]],
       newPassword: ["", [Validators.required]],
-
     });
+
   }
 
   async onCadastro() {
+    this.emailErrorMessage = "";
+    this.passwordErrorMessage = "";
+    this.sucessLogin = "";
+    this.incorrectCredentials = "";
+    this.nameErrorMessage = "";
+    this.newPasswordErrorMessage = "";
+
+    if (this.newUserLoginForm.value.name == "") {
+      this.nameErrorMessage = "O campo de nome e obrigatorio.";
+      return
+    }
+
+    if (this.newUserLoginForm.value.email == "") {
+      this.emailErrorMessage = "O campo de e-mail e obrigatorio.";
+      return
+    }
+
+    if (this.newUserLoginForm.value.password == "") {
+      this.passwordErrorMessage = "O campo de senha e obrigatorio.";
+    }else if(this.newUserLoginForm.value.password.length <= 6) {
+      this.passwordErrorMessage = "O campo de senha tem que ser maior que 6 caracter";
+    }
+     console.log("cheguei aqui ",this.newUserLoginForm.value.password.length)
+
+    if (this.newUserLoginForm.value.password != this.newUserLoginForm.value.newPassword) {
+      this.newPasswordErrorMessage = "A senha nao esta compativel .";
+      return
+    }
+
+    console.log("Email", this.newUserLoginForm.value.email)
+    console.log("Password", this.newUserLoginForm.value.password)
     
     const token = localStorage.getItem("meuToken")
     console.log("cheguei",token)
 
+    
     let response = await fetch("https://senai-gpt-api.azurewebsites.net/users", {
       method: "POST",
       headers: {
@@ -41,12 +74,23 @@ export class NewUserScreen {
         "Authorization": `Bearer ${token}`
       },
       body: JSON.stringify({
+        name: this.newUserLoginForm.value.name,
         email: this.newUserLoginForm.value.email,
         password: this.newUserLoginForm.value.password,
-        name: this.newUserLoginForm.value.name,
         newPassword: this.newUserLoginForm.value.newPassword,
+        
       }),
     });
-    console.log("cheguei aqui ", authGuard())
+
+    debugger
+
+    if (response.status >= 200 && response.status <= 299) {
+      this.sucessLogin = "Logado com sucesso.";
+      let json = await response.json();
+      console.log("JSON", json)
+      window.location.href = "login"
+    } else {
+      this.incorrectCredentials = "Credenciais incorretas";
+    }     
   }
 }
